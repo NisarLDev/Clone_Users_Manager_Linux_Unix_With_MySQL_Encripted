1ccee6642e1638d1f16868bdf0897433f06e0336
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2024-01-19 16:12:48 +0100

    Update

--- system.cpp
+++ system.cpp
@@ -9,5 +9,9 @@ int main()
 {
     int a, i = 0;
     string text, old, password0, password1, password2, pass, name, age, user, word, word1;
-    string
+    string creds[2], cp[2];
+
+    cout<<"  Security System Password"<<endl;
+    cout<<"                                    "<<endl<<endl;
+    cout<< "     Register                      "<<endl;
 }
