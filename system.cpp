9c8414875ca34aac04501b832cb3fd6bc96d29e2
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2024-01-19 18:05:57 +0100

    Add files via upload

--- system.cpp
+++ system.cpp
@@ -10,14 +10,14 @@ int main()
     int a, i = 0;
     string text, old, password0, password1, password2, pass, name, age, user, word, word1;
     string creds[2], cp[2];
-  
+    Menu:
     cout<<"  Security System Password"<<endl;
     cout<<"_____________________________________"<<endl;
-    cout<<"______|Menu Users Manager|___________"<<endl<<endl;
+    cout<<"______|Menu Users Manager|___________"<<endl;
     cout<< "|     1. Register                  |"<<endl;
     cout<< "|     2. Login                     |"<<endl;
     cout<< "|     3. Change password           |"<<endl;
-    cout<< "|_____4. Exit______________________|"<<endl;<<endl;
+    cout<< "|_____4. Exit______________________|"<<endl;
 
       do{
         cout<<endl<<endl;
@@ -46,7 +46,7 @@ int main()
                 }
                 
             break;
-            goto Menu:  
+            goto Menu;  
             }
             case 2:{
                 i=0;
