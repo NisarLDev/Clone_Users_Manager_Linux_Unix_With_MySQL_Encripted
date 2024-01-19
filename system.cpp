484bcf8525df74c784504a949c663583e3489c69
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2024-01-19 16:27:01 +0100

    Update

--- system.cpp
+++ system.cpp
@@ -17,5 +17,5 @@ int main()
     cout<< "|     1. Register                  |"<<endl;
     cout<< "|     2. Login                     |"<<endl;
     cout<< "|     3. Change password           |"<<endl;
-    cout<< "|     4. Exit                      |"<<endl;
+    cout<< "|_____4. Exit______________________|"<<endl;
 }
